69e86e4dda1835b9125a1f66214b1471676cc92e
shubham shinde <93381750+shubhamshinde01@users.noreply.github.com>
2024-06-12 00:03:19 +0530

    Update weather.js

--- scripjs/weather.js
+++ scripjs/weather.js
@@ -12,10 +12,10 @@ const Info=async(event)=>{
       try {
         let url= `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&unit=matric&appid=023a4657da503540a62e44f84ca58201`
         const responce = await fetch(url);
-        const data=await Sresponce.json();
+        const data=await responce.json();
         const arrData=[data];
-        console.log(arrData);
-        teamp.innerText=`${arrData[0].main.temp+sup}C`;
+        //console.log(arrData);
+        teamp.innerText=`${arrData[0].main.temp+sup}`;
         location.innerText=`${arrData[0].name},${arrData[0].sys.country}`;  
 
         console.log(data);
